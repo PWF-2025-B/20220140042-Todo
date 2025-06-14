59318d95e5a26463f198fef422388e4219b0cc46
abrarimamsatria09 <092004abrar@gmail.com>
2025-06-14 19:15:28 +0700

    api : logout, create, update, delete data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Gate;
@@ -32,8 +34,13 @@ class AppServiceProvider extends ServiceProvider
             return $user->is_admin == true;
         });
 
-        Scramble::configure()->routes(function (Route $route) {
-            return Str::startsWith($route->uri, 'api/');
-        });
+         Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            })->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
+            });;
     }
 }
---
+++ tugas12.md
@@ -0,0 +1,16 @@
+# Tugas 12
+
+## 1. Logout dan menghapus JW
+![alt text](/screenshot/tugas12/image.png)
+![alt text](/screenshot/tugas12/image2.png)
+![alt text](/screenshot/tugas12/image3.png)
+## 2. Create Data
+![alt text](/screenshot/tugas12/image4.png)
+## 3. Read data
+![alt text](/screenshot/tugas12/image5.png)
+## 4. Search Data
+![alt text](/screenshot/tugas12/image6.png)
+## 5. Update Data
+![alt text](/screenshot/tugas12/image7.png)
+## 6. Delete Data 
+![alt text](/screenshot/tugas12/image8.png)
